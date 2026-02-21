b5fba6f455dd0f76d7254b7da04709e194dbdb02
Vishal Jadhav <137301785+Vishaljadhav05@users.noreply.github.com>
2026-02-21 18:59:10 +0530

    String Concatenation And String Length

---
+++ 82 String Concatenation/String Concatenation.js
@@ -0,0 +1,11 @@
+var msg = "Hello, ";
+var name = "Vishal."
+
+//alert(msg + "there " +name)
+
+name.length;
+
+
+console.log(33);
+
+
---
+++ 83 String Length and Retriving the Number of Characters/String Length.js
@@ -0,0 +1,4 @@
+var tweet = prompt("Compose your Tweet..");
+var tweetCount = tweet.length;
+
+alert("You have written " + tweetCount + " Character you have " + (140 - tweetCount) + " Character Remaning..");
