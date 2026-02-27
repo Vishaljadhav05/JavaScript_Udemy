58ebd2580a3a6e0a4bfcf342209a430a46fda12d
Vishal Jadhav <137301785+Vishaljadhav05@users.noreply.github.com>
2026-02-27 22:28:15 +0530

    Functions in JS

---
+++ 89 Functions creating anc Calling/A_Syntax.js
@@ -0,0 +1,13 @@
+//Functions in JavaScript are reusable blocks of code designed to perform specific tasks.
+//They allow you to organize, reuse, and modularize code. It can take inputs, 
+//perform actions, and return outputs.
+
+function greet(name) {   // 'name' is a parameter
+  console.log("Hello " + name);
+}
+
+greet("Alice");  // "Alice" is the argument
+
+//output:-
+
+//Hello Alice
