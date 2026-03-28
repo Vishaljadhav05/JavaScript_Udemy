c92cbaaf85145b5609fe54a89bd7ce1c16d1ce99
Vishal Jadhav <137301785+Vishaljadhav05@users.noreply.github.com>
2026-03-28 17:36:57 +0530

    Comparators and challange and Array

---
+++ 103 104 Challange and Solution Leap Year/Leap.js
@@ -0,0 +1,38 @@
+function isLeap(year) {
+    
+/**************Don't change the code above****************/    
+    
+    //Write your code here. 
+
+    if(year % 4 === 0)
+    {
+        if(year % 100 === 0)
+        {
+            return 'Not leap year';
+        }
+        else
+        {
+            if(year % 400 === 0)
+            {
+                return 'Not leap year';
+            }
+            else
+            {
+                return 'Leap year.';
+            }
+        }
+    }
+    else
+    {
+        return 'Not leap year.';
+    }
+
+/**************Don't change the code below****************/    
+
+}
+
+console.log(isLeap(1948));
+
+// Output:-
+
+// Leap year.
---
+++ 105 Collection JS array/array.js
@@ -0,0 +1,15 @@
+var arr = ["Vishal","Prafull","Uday", "Komal"];
+
+console.log(arr);
+console.log(arr.length);
+console.log(arr.includes("Vj"));
+
+//Output:-
+
+/*
+
+[ 'Vishal', 'Prafull', 'Uday', 'Komal' ]
+4
+false
+
+*/
