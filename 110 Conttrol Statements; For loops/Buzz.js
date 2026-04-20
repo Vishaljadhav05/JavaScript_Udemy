ce161db960081ac21097aa69da2dedadb54e44bc
Vishal Jadhav <137301785+Vishaljadhav05@users.noreply.github.com>
2026-04-20 23:24:11 +0530

    For Loop & Fibonacci Series

---
+++ 110 Conttrol Statements; For loops/Buzz.js
@@ -0,0 +1,55 @@
+// Que :- write a program that prints the number from 1 to 20. But for multiples of 3 print "Fizz" instead of the number
+// and for the multiples of the of 5 print "Buzz." For numbers which are multiple of both 3 and 5 print "FizzBuzz"z
+
+
+let output = [];
+let count = 1;
+
+function fizzBuzz() {
+    // Code
+
+   for (let i = 0; i <= 100; i++) {
+    
+    if (count % 3 === 0 && count % 5 === 0 )
+    {
+        output.push("FizzBuzz");
+    }
+    else if(count % 3 === 0)
+    {
+        output.push("Fizz");
+    }
+    else if (count % 5 === 0 )
+    {
+        output.push("Buzz");
+    }
+
+    else{
+        output.push(count);
+    }
+    count++;
+        
+    }
+
+    console.log(output);
+
+    
+}
+
+fizzBuzz();
+
+
+
+// Output:-
+
+/*
+
+
+(100) [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz',
+16, 17, 'Fizz', 19, 'Buzz', 'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'FizzBuzz', 31,
+32, 'Fizz', 34, 'Buzz', 'Fizz', 37, 38, 'Fizz', 'Buzz', 41, 'Fizz', 43, 44, 'FizzBuzz', 46, 47,
+'Fizz', 49, 'Buzz', 'Fizz', 52, 53, 'Fizz', 'Buzz', 56, 'Fizz', 58, 59, 'FizzBuzz', 61, 62, 'Fizz',
+64, 'Buzz', 'Fizz', 67, 68, 'Fizz', 'Buzz', 71, 'Fizz', 73, 74, 'FizzBuzz', 76, 77, 'Fizz', 79, 'Buzz',
+'Fizz', 82, 83, 'Fizz', 'Buzz', 86, 'Fizz', 88, 89, 'FizzBuzz', 91, 92, 'Fizz', 94, 'Buzz', 'Fizz', 97,
+98, 'Fizz', 'Buzz']
+
+*/
---
+++ 112 Solution Of fibonacci/app.js
@@ -0,0 +1,45 @@
+function fibonacciGenerator (n) {
+//Do NOT change any of the code above 👆
+    
+    //Write your code here:
+    
+    let output = [];
+    if(n === 1)
+    {
+        output = [0];
+    }
+    
+    else if(n === 2)
+    {
+        output = [0,1];
+    }
+    else{
+        
+        output = [0,1];
+        for(let i =2 ;i<n ;i++)
+            {
+                output.push(output[output.length-2] + output[output.length-1]);
+            }
+    }
+    
+    
+    return output;
+    
+    
+    
+    //Return an array of fibonacci numbers starting from 0.
+    
+//Do NOT change any of the code below 👇
+}
+
+let output = fibonacciGenerator(9);
+
+console.log(output);
+
+// Output in console
+
+/*
+
+(9) [0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+*/
