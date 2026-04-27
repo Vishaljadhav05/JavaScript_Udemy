2493a3b42f09efd14c2a8717fda28046d034a747
Vishal Jadhav <137301785+Vishaljadhav05@users.noreply.github.com>
2026-04-27 17:47:36 +0530

    Manipulating Styles of HTML Elements
    Manipulating and Changing Styles of HTML Elements with Javascript
    The Separation of Concerns Structure vs Style vs Behaviour

---
+++ 118. Manipulating and Changing Styles of HTML Elements with Javascript/index.js
@@ -0,0 +1,5 @@
+document.querySelector("h1").style.color = "Orange";
+
+document.querySelector("h1").style.fontSize = "10rem";
+
+document.querySelector("button").style.backgroundColor = "yellow";
---
+++ 119. The Separation of Concerns Structure vs Style vs Behaviour/index.js
@@ -0,0 +1,7 @@
+document.querySelector("botton").classList.add("invisible");
+
+document.querySelector("button").classList.remove("invisible");
+
+document.querySelector("button").classList.toggle("invisible");
+
+document.querySelector("h1").classList.add("huge")
